09a9d5f69d1d21fd14934a540a10af84760117f2
giladyosef54 <57717590+giladyosef54@users.noreply.github.com>
2019-11-23 19:36:29 +0200

    strange changes
    strange changes in Date class

--- dotNet5780_02_3248_0250/dotNet5780_01_3248_0250/part 1/Date.cs
+++ dotNet5780_02_3248_0250/dotNet5780_01_3248_0250/part 1/Date.cs
@@ -4,11 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-<<<<<<< HEAD
-namespace part_1
-=======
-namespace part_1 //third test
->>>>>>> 4d4461db44514ae426e8d5351d2946c30d236282
+
+namespace part_1 
 {
     class Date  
     {
